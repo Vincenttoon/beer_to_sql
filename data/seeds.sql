12b10b56287798b529a36468d01e3ff017c3fe27
Vincent Toon <vincenttoon22@gmail.com>
2023-02-22 18:52:02 -0600

    some seeding

--- data/seeds.sql
+++ data/seeds.sql
@@ -1,4 +1,28 @@
-INSERT INTO beers (name, brewery, style, abv, rating, date_drunk, location, notes)
-VALUES (
-    ''
-)
+INSERT INTO breweries (name, location)
+VALUES 
+    ('Global Brew Tap House', 'Edwardsville'),
+    ('Global Brew Tap House', "O'Fallon");
+
+INSERT INTO ratings (value)
+VALUES
+    ('0.25')
+    ('0.50')
+    ('0.75')
+    ('1.00')
+    ('1.25')
+    ('1.50')
+    ('1.75')
+    ('2.00')
+    ('2.25')
+    ('2.50')
+    ('2.75')
+    ('3.00')
+    ('3.25')
+    ('3.50')
+    ('3.75')
+    ('4.00')
+    ('4.25')
+    ('4.50')
+    ('4.75')
+    ('5.00');
+
