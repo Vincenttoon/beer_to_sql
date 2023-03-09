c93a05f5d16a89dd54b7877f457b73a5384051f0
Vincent Toon <vincenttoon22@gmail.com>
2023-03-09 13:30:59 -0600

    inquirer running and can view all beers

--- data/index.js
+++ data/index.js
@@ -9,7 +9,13 @@ class DB {
     return this.connection
       .promise()
       .query(
-        "SELECT beers.id, beers.name, breweries.name, styles.name, beers.abv, ratings.value, beers.date_drunk, locations.name, beers.notes FROM beers JOIN ratings ON beers.rating_id = ratings.rating_id JOIN breweries ON beers.brewery_id = breweries.brewery_id JOIN breweries ON beers.brewery_id = breweries.brewery_id JOIN styles ON beers.style_id = styles.style_id JOIN locations ON beers.location_id = locations.location_id;"
+        `SELECT b.id, b.name, br1.name, s.name, b.abv, r.value, b.date_drunk, l.name, b.notes 
+        FROM beers b 
+        JOIN ratings r ON b.rating_id = r.rating_id 
+        JOIN breweries br1 ON b.brewery_id = br1.brewery_id 
+        JOIN styles s ON b.style_id = s.style_id 
+        JOIN locations l ON b.location_id = l.location_id 
+        JOIN breweries br2 ON b.brewery_id = br2.brewery_id;`
       );
   }
 
@@ -52,4 +58,112 @@ class DB {
         "SELECT ratings.value AS rating_value, beers.name AS beer_name, breweries.name AS brewery_name, styles.name AS style_name, beers.abv, locations.name AS location_name, beers.notes FROM beers JOIN breweries ON beers.brewery_id = breweries.brewery_id JOIN styles ON beers.style_id = styles.style_id JOIN ratings ON beers.rating_id = ratings.rating_id LEFT JOIN locations ON beers.location_id = locations.location_id ORDER BY rating_value DESC, beer_name, brewery_name, style_name, abv, location_name, notes"
       );
   }
+
+  viewBeersByBrewery(breweryName) {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT beers.id, beers.name, breweries.name AS brewery_name, styles.name AS style_name, beers.abv, ratings.value AS rating, beers.date_drunk, locations.name AS location_name, beers.notes FROM beers JOIN breweries ON beers.brewery_id = breweries.brewery_id JOIN styles ON beers.style_id = styles.style_id JOIN ratings ON beers.rating_id = ratings.rating_id LEFT JOIN locations ON beers.location_id = locations.location_id WHERE breweries.name = ? ORDER BY beers.name ASC;",
+        [breweryName]
+      );
+  }
+
+  seeBeersSingleByStyle(style) {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT * FROM beers JOIN styles ON beers.style_id = styles.style_id WHERE styles.name = ?",
+        [style]
+      );
+  }
+
+  seeBeersBySingleLocation(locationName) {
+    return this.connection.promise().query(
+      `SELECT beers.name, breweries.name, locations.name 
+        FROM beers 
+        JOIN breweries ON beers.brewery_id = breweries.id 
+        JOIN locations ON beers.location_id = locations.id 
+        WHERE locations.name = ?`,
+      [locationName]
+    );
+  }
+
+  seeBeersBySingleRating(rating) {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT beers.id, beers.name, breweries.name, styles.name, beers.abv, ratings.value, beers.date_drunk, locations.name, beers.notes FROM beers JOIN ratings ON beers.rating_id = ratings.rating_id JOIN breweries ON beers.brewery_id = breweries.brewery_id JOIN breweries ON beers.brewery_id = breweries.brewery_id JOIN styles ON beers.style_id = styles.style_id LEFT JOIN locations ON beers.location_id = locations.location_id WHERE ratings.value = ?;",
+        [rating]
+      );
+  }
+
+  addBrewery(name, city, state) {
+    return this.connection
+      .promise()
+      .query("INSERT INTO breweries (name, city, state) VALUES (?, ?, ?)", [
+        name,
+        city,
+        state,
+      ]);
+  }
+
+  addLocation(name, city, state) {
+    return this.connection
+      .promise()
+      .query("INSERT INTO locations (name, city, state) VALUES (?, ?, ?)", [
+        name,
+        city,
+        state,
+      ]);
+  }
+
+  addStyle(name) {
+    return this.connection
+      .promise()
+      .query("INSERT INTO styles (name) VALUES (?)", [name]);
+  }
+
+  addBeer(
+    name,
+    brewery_id,
+    style_id,
+    abv,
+    rating_id,
+    date_drunk,
+    location_id,
+    notes
+  ) {
+    return this.connection
+      .promise()
+      .query(
+        "INSERT INTO beers (name, brewery_id, style_id, abv, rating_id, date_drunk, location_id, notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+          name,
+          brewery_id,
+          style_id,
+          abv,
+          rating_id,
+          date_drunk,
+          location_id,
+          notes,
+        ]
+      );
+  }
+
+  updateBeerRating(beerId, newRating) {
+    return this.connection
+      .promise()
+      .query("UPDATE beers SET rating_id = ? WHERE id = ?", [
+        newRating,
+        beerId,
+      ]);
+  }
+
+  deleteBeer(id) {
+    return this.connection
+      .promise()
+      .query("DELETE FROM beers WHERE id = ?", [id]);
+  }
 }
+
+module.exports = new DB(connection);
--- index.js
+++ index.js
@@ -1,135 +1,8 @@
 const connection = require("./data/connection");
 const inquirer = require("inquirer");
-const { AutocompletePrompt } = require("inquirer-autocomplete-prompt");
+const { AutocompletePrompt } = import("inquirer-autocomplete-prompt");
 const db = require("./data");
 require("console.table");
-
-function getBreweryNames() {
-  return new Promise((resolve, reject) => {
-    // Execute a query to retrieve the brewery names from the database
-    // and pass the results to the `resolve` function
-    connection.query("SELECT name FROM breweries", (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        // Map the results to an array of brewery names and pass it to the `resolve` function
-        const breweryNames = results.map((result) => result.name);
-        resolve(breweryNames);
-      }
-    });
-  });
-}
-
-// function to get all locations
-function getLocations() {
-  return new Promise((resolve, reject) => {
-    connection.query("SELECT * FROM locations", (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        const locations = results.map((result) => result.name);
-        resolve(locations);
-      }
-    });
-  });
-}
-
-// function to get all ratings
-function getRatings() {
-  return new Promise((resolve, reject) => {
-    connection.query("SELECT * FROM ratings", (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        const ratings = results.map((result) => result.value);
-        resolve(ratings);
-      }
-    });
-  });
-}
-
-// function to get all styles
-function getStyles() {
-  return new Promise((resolve, reject) => {
-    connection.query("SELECT * FROM styles", (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        const styles = results.map((result) => result.name);
-        resolve(styles);
-      }
-    });
-  });
-}
-
-// Define a prompt function for each type of data
-const promptBrewery = async () => {
-  const response = await inquirer.prompt([
-    {
-      type: "autocomplete",
-      name: "brewery",
-      message: "Select brewery:",
-      source: async (answersSoFar, input) => {
-        const results = breweries.filter((option) =>
-          option.toLowerCase().includes(input.toLowerCase())
-        );
-        return results;
-      },
-    },
-  ]);
-  return response.brewery;
-};
-
-const promptLocation = async () => {
-  const response = await inquirer.prompt([
-    {
-      type: "autocomplete",
-      name: "location",
-      message: "Select location:",
-      source: async (answersSoFar, input) => {
-        const results = locations.filter((option) =>
-          option.toLowerCase().includes(input.toLowerCase())
-        );
-        return results;
-      },
-    },
-  ]);
-  return response.location;
-};
-
-const promptRating = async () => {
-  const response = await inquirer.prompt([
-    {
-      type: "autocomplete",
-      name: "rating",
-      message: "Select rating:",
-      source: async (answersSoFar, input) => {
-        const results = ratings.filter((option) =>
-          option.toLowerCase().includes(input.toLowerCase())
-        );
-        return results;
-      },
-    },
-  ]);
-  return response.rating;
-};
-
-const promptStyle = async () => {
-  const response = await inquirer.prompt([
-    {
-      type: "autocomplete",
-      name: "style",
-      message: "Select style:",
-      source: async (answersSoFar, input) => {
-        const results = styles.filter((option) =>
-          option.toLowerCase().includes(input.toLowerCase())
-        );
-        return results;
-      },
-    },
-  ]);
-  return response.style;
-};
 
 const init = () => {
   mainMenu();
@@ -161,6 +34,9 @@ const mainMenu = () => {
         case "View all beers":
           viewAllBeers();
           break;
+        case "Quit":
+          quit();
+          break;
       }
     });
 };
@@ -175,77 +51,9 @@ const viewAllBeers = () => {
     .then(() => mainMenu());
 };
 
-async function addBeer() {
-  // Prompt user for beer information
-  const beerName = await promptBeerName();
-  const breweryId = await promptBrewery();
-  const style = await promptStyle();
-  const abv = await promptAbv();
-  const location = await promptLocation();
-  const notes = await promptNotes();
-  const ratingId = await promptRating();
-
-  // Check if the brewery already exists, add it if it does not
-  const [existingBrewery] = await db.query(
-    "SELECT id FROM breweries WHERE name = ?",
-    [breweryId]
-  );
-  if (!existingBrewery) {
-    const result = await db.query("INSERT INTO breweries (name) VALUES (?)", [
-      breweryId,
-    ]);
-    breweryId = result.insertId;
-  }
-
-  // Check if the location already exists, add it if it does not
-  let existingLocation = null;
-  if (location) {
-    [existingLocation] = await db.query(
-      "SELECT id FROM locations WHERE name = ?",
-      [location]
-    );
-    if (!existingLocation) {
-      const result = await db.query("INSERT INTO locations (name) VALUES (?)", [
-        location,
-      ]);
-      existingLocation = {
-        id: result.insertId,
-        name: location,
-      };
-    }
-  }
-
-  // Check if the style already exists, add it if it does not
-  let existingStyle = null;
-  if (style) {
-    [existingStyle] = await db.query("SELECT id FROM styles WHERE name = ?", [
-      style,
-    ]);
-    if (!existingStyle) {
-      const result = await db.query("INSERT INTO styles (name) VALUES (?)", [
-        style,
-      ]);
-      existingStyle = {
-        id: result.insertId,
-        name: style,
-      };
-    }
-  }
-
-  // Insert the new beer into the database
-  const result = await db.query(
-    "INSERT INTO beers (name, brewery_id, style_id, abv, location_id, notes, rating_id) VALUES (?, ?, ?, ?, ?, ?, ?)",
-    [
-      beerName,
-      existingBrewery.id,
-      existingStyle ? existingStyle.id : null,
-      abv,
-      existingLocation ? existingLocation.id : null,
-      notes,
-      ratingId,
-    ]
-  );
-  console.log(`Added ${beerName} to the database!`);
-}
+const quit = () => {
+  console.log("Goodbye!");
+  process.exit();
+};
 
 init();
